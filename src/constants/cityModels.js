c037afd13289421caa01ddbbc06c86da4a6bde18
Arren Alexander <aralx73@gmail.com>
2019-06-23 13:04:41 -0600

    closes #4 closes #5 fix: redux devtools, object lookups

---
+++ src/constants/cityModels.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const cityModels = {
+    "Paris": <a-entity collada-model="url(/models/eiffeltower.dae)" scale=".1 .1 .1" position="-2 6 -18" rotation="0 45 0"></a-entity>,
+    "New York City": <a-entity collada-model="url(/models/empirestate.dae)" scale="3 3 3" position="-2 9.2 -18" rotation="0 45 0"></a-entity>,
+    "Denver": <a-entity collada-model="url(/models/mountain.dae)" scale=".25 .25 .25" position="-10 5.5 -40" rotation="0 145 0"></a-entity>,
+    "London": <a-entity collada-model="url(/models/BigBen/model.dae)" scale=".25 .25 .25" position="-15 0 -30" rotation="0 -90 0"></a-entity>,
+    "Dubai": <a-entity collada-model="url(/models/Burj/model.dae)" scale=".08 .08 .08" position="-15 0 -30" rotation="0 -45 0"></a-entity>,
+    "Seattle": <a-entity collada-model="url(/models/spaceneedle.dae)" scale=".7 .7 .7" position="40 0 -40" rotation="0 135 0"></a-entity>,
+    "Honolulu": <a-entity collada-model="url(/models/coconut.dae)" scale="50 50 50" position="-30 0 -55" rotation="0 145 0"></a-entity>,
+    "Sao Paulo": <a-entity collada-model="url(/models/SaoPaulo/model.dae)" scale=".5 .5 .5" position="-30 0 -55" rotation="0 60 0"></a-entity>,
+    "Istanbul": <a-entity collada-model="url(/models/Istanbul/model.dae)" scale=".5 .5 .5" position="-30 0 -55" rotation="0 60 0"></a-entity>,
+    "Jakarta": <a-entity collada-model="url(/models/jakarta.dae)" scale=".5 .5 .5" position="-30 0 -55" rotation="0 60 0"></a-entity>,
+    "Cairo": <a-entity collada-model="url(/models/sphinx/model.dae)" scale="10 10 10" position="-30 0 -55" rotation="0 30 0"></a-entity>,
+}
---
+++ src/constants/iconConstant.js
@@ -0,0 +1,68 @@
+export const iconConstant = {
+  "fog": {
+    primaryColor: '#797278',
+    secondaryColor: '#789767',
+    numClouds: 8,
+    rainCount: "count: 0;"
+  },
+  "cloudy": {
+    primaryColor: '#797278',
+    secondaryColor: '#789767',
+    numClouds: 8,
+    rainCount: "count: 0;"
+  },
+  "partly-cloudy-day": {
+    primaryColor: '#797278',
+    secondaryColor: '#789767',
+    numClouds: 6,
+    rainCount: "count: 0;"
+  },
+  "partly-cloudy-night": {
+    primaryColor: '#797278',
+    secondaryColor: '#789767',
+    numClouds: 6,
+    rainCount: "count: 0;"
+  },
+  "clear-day":  {
+    primaryColor: '#425E44',
+    secondaryColor: '#789767',
+    numClouds: 1,
+    rainCount: "count: 0;"
+  },
+  "clear-night": {
+    primaryColor: '#425E44',
+    secondaryColor: '#789767',
+    numClouds: 1,
+    rainCount: "count: 0;"
+  },
+  "wind": {
+    primaryColor: '#425E44',
+    secondaryColor: '#789767',
+    numClouds: 2,
+    rainCount: "count: 0;"
+  },
+  "snow": {
+    primaryColor: '#E7EBF0',
+    secondaryColor: '#E9F6F6',
+    numClouds: 5,
+    rainCount: "dropRadius: 0.08; dropHeight: 0.1; vector: 0 -2 0; opacity: .8; splashBounce: 0.8; count: 4000; color: #E7EBF0; splashGravity: 1.6;"
+  },
+  "sleet": {
+    primaryColor: '#E7EBF0',
+    secondaryColor: '#E9F6F6',
+    numClouds: 5,
+    rainCount: "dropRadius: 0.08; dropHeight: 0.1; vector: 0 -2 0; opacity: .8; splashBounce: 0.8; count: 4000; color: #E7EBF0; splashGravity: 1.6;"
+  },
+  "rain": {
+    primaryColor: '#C1C8D9',
+    secondaryColor: '#D8E9EE',
+    numClouds: 6,
+    rainCount: "count: 4000;"
+  },
+  'default': {
+    primaryColor: '#425E44',
+    secondaryColor: '#789767',
+    numClouds: 4,
+    rainCount: "count: 0;"
+  }
+}
